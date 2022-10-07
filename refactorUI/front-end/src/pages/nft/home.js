c1517b54578654da784ee8f34f6fc3cc67d431db
Pysavant Codes <110984357+pysavantcodes@users.noreply.github.com>
2022-10-07 16:17:23 +0100

    Update home.js

--- refactorUI/front-end/src/pages/nft/home.js
+++ refactorUI/front-end/src/pages/nft/home.js
@@ -29,7 +29,9 @@ const Container = () => {
               brand and products.
             </p>
             <div className="button-group">
-              <NavLink style={{color:"white"}} to={`/nft/${isConnected?"create":"explore"}`}><button className="btn">{(isConnected?"Create":"Explore")} NFT</button></NavLink>
+              <button className="btn"><NavLink style={{color:"white"}} to={`/nft/${isConnected?"create":"explore"}`}>{(isConnected?"Create":"Explore")} NFT</NavLink></button>
+
+
             </div>
           </div>
         </div>
