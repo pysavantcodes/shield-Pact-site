02d5a90a9341b4d5406c8278787684c8a4c100ee
Pysavant Codes <110984357+pysavantcodes@users.noreply.github.com>
2022-10-22 12:59:08 +0100

    Update App.js

--- refactorUI/front-end/src/App.js
+++ refactorUI/front-end/src/App.js
@@ -35,7 +35,7 @@ const router = createBrowserRouter([
 					},
 					{
 						path:'',//default
-						element:<Navigate to='launchpad'/>,
+						element:<Navigate to='swap'/>,
 					},
 					{
 						path:'staking',
