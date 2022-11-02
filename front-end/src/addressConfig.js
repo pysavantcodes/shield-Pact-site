3afba31d850790600fbbb0dca3fcab60e1ac761b
Pysavant Codes <110984357+pysavantcodes@users.noreply.github.com>
2022-11-03 00:47:54 +0100

    Add files via upload

--- front-end/src/addressConfig.js
+++ front-end/src/addressConfig.js
@@ -2,31 +2,30 @@
 
 const extraOption = {
 	/*Wallet Connect api key */
-  PROJECTID:'5f0a0c1eda156a9eade6dfcf6dbc0cea',
+  PROJECTID:'37fa85961b590aed7fa818a43f4e622c',
   /**NFT storage bucket api key*/
-  NFT_STORAGE_KEY:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDEyQjhiNzkxQzkxMDNlMUNEMDU0RWU0Nzc5MkQ2NDY3OTQ0YjFkYTUiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2NTA0MTIzNTQwMywibmFtZSI6IlNISUVMRENPSU4ifQ.bBacconKCT3IaZkLAUPpss7BIdZBjcU0QTIvqRHS9XQ',
+  NFT_STORAGE_KEY:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDE5ODExMEY3RjZiRjUyMzRjODdEYWQ4ZGRhMkVlNEQzZEFBQTM2NTciLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2NzQzMjM2MDkxMywibmFtZSI6InNoaWVsZHBhY3QifQ.mmETnMHz1fPjjqpAUD2qiGE_2XqLWVVC_RNxUGGxbTI',
   /**Admin address*/
-  admin:'0x712196554d705f00396b9cB7D8384D225E92DF1b',
+  admin:'0xd1a5444F99BE6C3EefBc6998c5e7F0F069025d98',
   /**launchpad Factory Address*/
-  launchFactoryAddress:"0x6A928b2d58fCaF04aC957CE318810f04f87a7EFb",
+  launchFactoryAddress:"0x4C90f5C6fADc7a2E6a79877818035c995c28D21b", /**deployed launch address, testAddress: 0x6A928b2d58fCaF04aC957CE318810f04f87a7EFb**/
   /**Nft Address**/
-  nftAddress:"0x9df5A067273096A4fa38CFF9dB33161355Cb6154",
+  nftAddress:"0xeB032426920182A3686B25Dc85a555Bf27b48C62", /**deployed nft address, testAddress: 0x9df5A067273096A4fa38CFF9dB33161355Cb6154**/
   /**MarketPlace Address**/
-  marketAddress:"0x57EB5a1AcaC54071062aF2289945954cb12C8a84",
+  marketAddress:"0x51dc5D90e2779fB2cFF9a95b579075A93a8a837C", /**deployed market address, testAddress: 0x57EB5a1AcaC54071062aF2289945954cb12C8a84**/
   /**Staking Address**/
-  stakeAddr:"0x6f2326E0cB5def734Cee56fbFb9401D1F982447C",
+  stakeAddr:"0x9cA286914F0A195000BE999E1384659Ef5a50207",/**deployed stake address, testAddress: 0x6f2326E0cB5def734Cee56fbFb9401D1F982447C**/
   /**AirDrop Address*/
-  airDropAddress:"0x2F0dD0492B0D3d0dc694d7BbA491C7d9aBA8fE18",
+  airDropAddress:"0x8426E76219bA09Da03A1fb0480e4F2cF1e12a5c8",/**deployed airdop address**/
   /**Token Factory Address**/
-  tokenFactoryAddress:"0x0AED08168aE1Aa0E363877D34BD7b094Bc7e4f0b",
+  tokenFactoryAddress:"0x8579DD6be71b26A88F326Ee6Ae1759a362e24CD7",/**deployed tokenfactory address, testAddress: 0x0AED08168aE1Aa0E363877D34BD7b094Bc7e4f0b**/
   /**Swap Factory Address**/
-  swapAddress:"0xd2c95dD3709BB4531cEE0C8C3EDE3819f6505974",
+  swapAddress:"0xb9C31aF0030db6728097a2f450B6F11ec2602328",/**deployed swap address, testAddress: 0xd2c95dD3709BB4531cEE0C8C3EDE3819f6505974**/
   /**SHC PACT ADDRESS MUST BE SPECIFIED HERE**/
-  bonusTokenAddress:false,
+  bonusTokenAddress:"0x4E6345e00B8DA9b12dcF81D585D15Ea9924B3E80",
   /**Token Server Address for Webserver Directory**/
   tokenServerAddress:"https://glacial-peak-07030.herokuapp.com/",//deployed webserver address here
   /**ton be specified for production Evnvironment**/
-  production:false
+  production:true
 }
-
 export default extraOption;
